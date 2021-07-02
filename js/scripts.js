89eb0949212bd4f4b1b037590cd7e4b4bc6bc76a
Mary Marks <maryleemarks@gmail.com>
2021-07-02 13:19:54 -0700

    return an array with 0 if 0 is inputted

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// Business Logic
+
+function beepBoop(text) [
+  for (let i=0; i <=inputNumber; i++) {
+    
+  }
+]
+
+// UI Logic
+
+$(document).ready(function() {
+  $("form#input").submit(function(event) {
+
+    const inputNumber = parseInt($("input#inputNumber").val());
+    event.preventDefault();
+  }
+    $("#response").show();
+  });
+});
